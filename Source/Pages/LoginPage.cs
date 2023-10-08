effcf0be8edd7d21154e08a8e59930a476bbb6d9
Kalani Wijayawardana <S361062@students.cdu.edu.au>
2023-10-08 21:35:12 +0930

    Add project files.

---
+++ Source/Pages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Source.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Id, Using = "username_id")]
+        private IWebElement _usernameTextBox;
+
+        [FindsBy(How = How.Id, Using = "password_id")]
+        private IWebElement _passwordTextBox;
+
+        [FindsBy(How = How.Id, Using = "login-btn")]
+        private IWebElement _signinButton;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Login()
+        {
+            _usernameTextBox.SendKeys("");
+            _passwordTextBox.SendKeys("");
+            _signinButton.Click();
+        }
+    }
+}
---
+++ Test/LoginTest.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumProject.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumProject.Test
+{
+    public class LoginTest
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitScript()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void SignIn()
+        {
+            //LoginPage loginPage = new LoginPage(IWebDriver);
+            _driver.Navigate().GoToUrl("");
+           // loginPage.Login();
+        }
+    }
+}
